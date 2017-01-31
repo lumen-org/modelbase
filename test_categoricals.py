bc29c0519bef101011c7e93d0e9d68886fb7df33
Philipp Lucas <philipp.lucas@uni-jena.de>
2017-01-31 15:20:30 +0100

    created basic tests for categoricals.py

---
+++ test_categoricals.py
@@ -0,0 +1,125 @@
+# Copyright (c) 2017 Philipp Lucas (philipp.lucas@uni-jena.de)
+"""
+@author: Philipp Lucas
+
+Test Suite for categoricals.py
+"""
+
+import unittest
+import numpy.testing as npt
+import numpy as np
+import pandas as pd
+
+from categoricals import CategoricalModel as CategoricalModel
+
+
+class TestRunningShouldNotRaiseException(unittest.TestCase):
+    """ This is not a real test case... it's just a couple of model queries that should go through
+     without raising any exception """
+
+    def test_1(self):
+        df = pd.read_csv('data/categorical_dummy.csv')
+        model = CategoricalModel('test')
+        model.fit(df)
+
+        print('model:', model)
+        print('model._p:', model._p)
+
+        res = model.predict(
+            predict=['Student', 'City', 'Sex',
+                    (['City', 'Sex', 'Student'], 'density', 'density', [])],
+            splitby=[('City', 'elements', []),('Sex', 'elements', []),
+                    ('Student', 'elements', [])])
+        print('probability table: \n', str(res))
+
+        res = model.predict(
+            predict=['City', 'Sex',
+                    (['City', 'Sex'], 'density', 'density', [])],
+            splitby=[('City', 'elements', []),('Sex', 'elements', [])])
+        print("\n\npredict marginal table: city, sex\n" + str(res))
+
+        res = model.predict(
+            predict=['City',
+                    (['Sex'], 'maximum', 'Sex', [])],
+            splitby=[('City', 'elements', [])])
+        print("\n\npredict most likely sex by city: \n" + str(res))
+
+        res = model.predict(
+            predict=['Sex',
+                    (['Sex'], 'density', 'density', [])],
+            splitby=[('Sex', 'elements', [])])
+        print("\n\npredict marginal table: sex\n" + str(res))
+
+        res = model.predict(
+            predict=['City', 'Sex',
+                    (['City', 'Sex'], 'density', 'density', [])],
+            splitby=[('City', 'elements', []),('Sex', 'elements', [])],
+            where=[('Student', '==', 'yes')])
+        print("\n\nconditional prop table: p(city, sex| student=T):\n" + str(res))
+
+        print('\n\nafter this comes less organized output:')
+        print('model density 1:', model._density(['Jena', 'M', 'no']))
+        print('model density 1:', model._density(['Erfurt', 'F', 'yes']))
+
+        print('model maximum:', model._maximum())
+
+        marginalAB = model.copy().marginalize(keep=['City', 'Sex'])
+        print('marginal on City, Sex:', marginalAB)
+        print('marginal p: ', marginalAB._p)
+
+        conditionalB = marginalAB.condition([('City', '==', 'Jena')]).marginalize(keep=['Sex'])
+        print('conditional Sex|City = Jena: ', conditionalB)
+        print('conditional Sex|City = Jena: ', conditionalB._p)
+
+        print('most probable city: ', model.copy().marginalize(keep=['City']).aggregate('maximum'))
+        print('most probably gender in Jena: ',
+              model.copy().condition([('City', '==', 'Jena')]).marginalize(keep=['Sex']).aggregate('maximum'))
+        print('most probably Sex for Students in Erfurt: ',
+              model.copy().condition([('City', '==', 'Erfurt'), ('Student', '==', 'yes')]).marginalize(keep=['Sex']).aggregate(
+                  'maximum'))
+
+
+# class TestInferenceLvl01(unittest.TestCase):
+#     def setUp(self):
+#         # setup model with zero mean and identiy matrix as sigma
+#         self.m = Gaussian("test")
+#         self.m._generate_model(opts={'mode': 'normal', 'dim': 5})
+#
+#     def test_marginalize_1(self):
+#         m = self.m
+#         m.model(model=m.names[1:4])
+#         dim = m._n
+#         npt.assert_equal(m._S, np.eye(dim))
+#         npt.assert_equal(m._mu, np.matrix(np.zeros(dim)).T)
+#
+#         m.model(model=m.names[0:2])
+#         dim = m._n
+#         npt.assert_equal(m._S, np.eye(dim))
+#         npt.assert_equal(m._mu, np.matrix(np.zeros(dim)).T)
+#
+#     def test_condition_1(self):
+#         m = self.m
+#         m.model(model='*', where=[('dim1', '==', 0)])
+#         dim = m._n
+#         npt.assert_equal(m._S, np.eye(dim))
+#         npt.assert_equal(m._mu, np.matrix(np.zeros(dim)).T)
+#
+#         m.model(model='*', where=[('dim3', '==', 0), ('dim4', '==', 0)])
+#         dim = m._n
+#         npt.assert_equal(m._S, np.eye(dim))
+#         npt.assert_equal(m._mu, np.matrix(np.zeros(dim)).T)
+#
+#     def test_maximum_1(self):
+#         m = self.m
+#         res = m.aggregate('maximum')
+#         npt.assert_equal(res, np.zeros(m._n))
+#
+#     def test_predict_1(self):
+#         m = self.m
+#         res = m.aggregate('maximum')
+#         npt.assert_equal(res, np.zeros(m._n))
+
+
+if __name__ == '__main__':
+    unittest.main()
+
--- test_gaussians.py
+++ test_gaussians.py
@@ -2,7 +2,7 @@
 """
 @author: Philipp Lucas
 
-Test Suite for cond_gaussians.py
+Test Suite for gaussians.py
 """
 
 import unittest
@@ -16,79 +16,77 @@ class TestRunningShouldNotRaiseException(unittest.TestCase):
     """ This is not a real test case... it's just a couple of model queries that should go through
      without raising any exception """
 
-    # foo = MultiVariateGaussianModel.normalMVG(5,"foo")
-    sigma = np.matrix([
-        [1.0, 0.6, 0.0, 2.0],
-        [0.6, 1.0, 0.4, 0.0],
-        [0.0, 0.4, 1.0, 0.0],
-        [2.0, 0.0, 0.0, 1.]])
-    mu = np.matrix([1.0, 2.0, 0.0, 0.5]).T
-    foo = Gaussian("foo")
-    opts = {'mode': 'custom', 'sigma': sigma, 'mu': mu}
-    foo._generate_model(opts)
-    foocp = foo.copy("foocp")
-    print("\n\nmodel 1\n" + str(foocp))
-    foocp2 = foocp.model(['dim1', 'dim0'], as_="foocp2")
-    print("\n\nmodel 2\n" + str(foocp2))
+    def test_1(self):
+        sigma = np.matrix([
+            [1.0, 0.6, 0.0, 2.0],
+            [0.6, 1.0, 0.4, 0.0],
+            [0.0, 0.4, 1.0, 0.0],
+            [2.0, 0.0, 0.0, 1.]])
+        mu = np.matrix([1.0, 2.0, 0.0, 0.5]).T
+        foo = Gaussian("foo")
+        opts = {'mode': 'custom', 'sigma': sigma, 'mu': mu}
+        foo._generate_model(opts)
+        foocp = foo.copy("foocp")
+        print("\n\nmodel 1\n" + str(foocp))
+        foocp2 = foocp.model(['dim1', 'dim0'], as_="foocp2")
+        print("\n\nmodel 2\n" + str(foocp2))
 
-    res = foo.predict(predict=['dim0'], splitby=[('dim0', 'equidist', [5])])
-    print("\n\npredict 1\n" + str(res))
+        res = foo.predict(predict=['dim0'], splitby=[('dim0', 'equidist', [5])])
+        print("\n\npredict 1\n" + str(res))
 
-    res = foo.predict(predict=[(['dim1'], 'maximum', 'dim1', []), 'dim0'],
-                      splitby=[('dim0', 'equidist', [10])])
-    print("\n\npredict 2\n" + str(res))
+        res = foo.predict(predict=[(['dim1'], 'maximum', 'dim1', []), 'dim0'],
+                          splitby=[('dim0', 'equidist', [10])])
+        print("\n\npredict 2\n" + str(res))
 
-    res = foo.predict(predict=[(['dim0'], 'maximum', 'dim0', []), 'dim0'],
-                      where=[('dim0', 'equals', 1)], splitby=[('dim0', 'equidist', [10])])
-    print("\n\npredict 3\n" + str(res))
+        res = foo.predict(predict=[(['dim0'], 'maximum', 'dim0', []), 'dim0'],
+                          where=[('dim0', 'equals', 1)], splitby=[('dim0', 'equidist', [10])])
+        print("\n\npredict 3\n" + str(res))
 
-    res = foo.predict(predict=[(['dim0'], 'density', 'dim0', []), 'dim0'],
-                       splitby=[('dim0', 'equidist', [10])])
-    print("\n\npredict 4\n" + str(res))
+        res = foo.predict(predict=[(['dim0'], 'density', 'dim0', []), 'dim0'],
+                           splitby=[('dim0', 'equidist', [10])])
+        print("\n\npredict 4\n" + str(res))
 
-    res = foo.predict(
-        predict=[(['dim0'], 'density', 'dim0', []), 'dim0'],
-        splitby=[('dim0', 'equidist', [10])],
-        where=[('dim0', 'greater', -1)])
-    print("\n\npredict 5\n" + str(res))
+        res = foo.predict(
+            predict=[(['dim0'], 'density', 'dim0', []), 'dim0'],
+            splitby=[('dim0', 'equidist', [10])],
+            where=[('dim0', 'greater', -1)])
+        print("\n\npredict 5\n" + str(res))
 
-    res = foo.predict(
-        predict=[(['dim0'], 'density', 'dim0', []), 'dim0'],
-        splitby=[('dim0', 'equidist', [10])],
-        where=[('dim0', 'less', -1)])
-    print("\n\npredict 6\n" + str(res))
+        res = foo.predict(
+            predict=[(['dim0'], 'density', 'dim0', []), 'dim0'],
+            splitby=[('dim0', 'equidist', [10])],
+            where=[('dim0', 'less', -1)])
+        print("\n\npredict 6\n" + str(res))
 
-    res = foo.predict(
-        predict=[(['dim0'], 'density', 'dim0', []), 'dim0'],
-        splitby=[('dim0', 'equidist', [10])],
-        where=[('dim0', 'less', 0), ('dim2', 'equals', -5.0)])
-    print("\n\npredict 7\n" + str(res))
+        res = foo.predict(
+            predict=[(['dim0'], 'density', 'dim0', []), 'dim0'],
+            splitby=[('dim0', 'equidist', [10])],
+            where=[('dim0', 'less', 0), ('dim2', 'equals', -5.0)])
+        print("\n\npredict 7\n" + str(res))
 
-    res, base = foo.predict(
-        predict=[(['dim0'], 'density', 'dim0', []), 'dim0'],
-        splitby=[('dim0', 'equidist', [10]), ('dim1', 'equidist', [7])],
-        where=[('dim0', 'less', -1), ('dim2', 'equals', -5.0)],
-        returnbasemodel=True)
-    print("\n\npredict 8\n" + str(res))
+        res, base = foo.predict(
+            predict=[(['dim0'], 'density', 'dim0', []), 'dim0'],
+            splitby=[('dim0', 'equidist', [10]), ('dim1', 'equidist', [7])],
+            where=[('dim0', 'less', -1), ('dim2', 'equals', -5.0)],
+            returnbasemodel=True)
+        print("\n\npredict 8\n" + str(res))
 
-    res, base = foo.predict(
-        predict=[(['dim0'], 'average', 'dim0', []), (['dim0'], 'density', 'dim0', []),
-                 'dim0'],
-        splitby=[('dim0', 'equidist', [10])],
-        # where=[('dim0', 'less', -1), ('dim2', 'equals', -5.0)],
-        returnbasemodel=True)
-    print("\n\npredict 9\n" + str(res))
+        res, base = foo.predict(
+            predict=[(['dim0'], 'average', 'dim0', []), (['dim0'], 'density', 'dim0', []),
+                     'dim0'],
+            splitby=[('dim0', 'equidist', [10])],
+            # where=[('dim0', 'less', -1), ('dim2', 'equals', -5.0)],
+            returnbasemodel=True)
+        print("\n\npredict 9\n" + str(res))
 
-    res, base = foo.predict(
-        predict=['dim0', 'dim1', (['dim0', 'dim1'], 'average', 'dim0', []),
-                 (['dim0', 'dim1'], 'average', 'dim1', [])],
-        splitby=[('dim0', 'identity', []), ('dim1', 'equidist', [4])],
-        where=[('dim0', '<', 2), ('dim0', '>', 1)],
-        returnbasemodel=True)
-    print("\n\npredict 10\n" + str(res))
+        res, base = foo.predict(
+            predict=['dim0', 'dim1', (['dim0', 'dim1'], 'average', 'dim0', []),
+                     (['dim0', 'dim1'], 'average', 'dim1', [])],
+            splitby=[('dim0', 'identity', []), ('dim1', 'equidist', [4])],
+            where=[('dim0', '<', 2), ('dim0', '>', 1)],
+            returnbasemodel=True)
+        print("\n\npredict 10\n" + str(res))
 
-    # print("\n\n" + str(base) + "\n")
-    
 
 class TestInferenceLvl01(unittest.TestCase):
     
