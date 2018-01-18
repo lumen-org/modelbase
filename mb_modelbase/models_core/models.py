23ec488a12d504d68dc424044b899a3583dfa5f2
Philipp Lucas <philipp.lucas@uni-jena.de>
2018-01-18 10:29:20 +0100

    improved speed by improved data frame iteration

--- mb_modelbase/models_core/models.py
+++ mb_modelbase/models_core/models.py
@@ -1335,10 +1335,10 @@ class Model:
                 except KeyError as err:
                     raise ValueError("missing split-clause for field '" + str(err) + "'.")
                 subframe = input_frame[ids]
-                # TODO: iterrows is slow! better use itertuples!
-                # TODO2: also change interface: either list of values in order, or a dict. also fits better to itertuples
-                for _, row in subframe.iterrows():
-                    res = aggr_model.density(names, list(row))
+
+                # TODO: change interface: either list of values in order, or a dict.
+                for row in subframe.itertuples(index=False):
+                    res = aggr_model.density(names, row)
                     aggr_results.append(res)
 
                 # TODO: the normalization is incorrect: it must not normalize to 1, but to some slice
