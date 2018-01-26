e85f2b189702dfc2838f628bbd12f557fb24d723
Julien Klaus <julien.klaus@uni-jena.de>
2018-01-26 09:11:51 +0100

    optimize multiply times to get possible the global max

--- mb_modelbase/models_core/spnmodel.py
+++ mb_modelbase/models_core/spnmodel.py
@@ -78,11 +78,17 @@ class SPNModel(Model):
             raise Exception("Two many values.")
         return np.exp(self._spnmodel.evaluate(None, tmp))[0]
 
-    def _maximum(self):
+    # calculated iterations times the maximum and returns the position 
+    # with the highest densitiy value
+    def _maximum(self, iterations=10):
         fun = lambda x :-1 * self._density(x)
         xlength = sum(1 for x in self.index.values() if x is None)
-        x0 = np.random.randn(xlength)
-        xmax = minimize(fun, x0, method='Nelder-Mead')
+        xmax = None
+        for i in range(iterations):
+           x0 = np.random.randn(xlength)
+           xopt = minimize(fun, x0, method='Nelder-Mead')
+           if xmax is None or self._density(xmax) <=  self._density(xopt.x):
+              xmax = xopt.x
         return xmax
 
     def copy(self, name=None):
