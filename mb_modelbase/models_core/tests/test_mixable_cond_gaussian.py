1901a619fcf437e85b0bc2f8567a87f5057f9791
Christoph Saffer <christoph.saffer@uni-jena.de>
2018-05-22 14:47:03 +0200

    Added some new tests

--- mb_modelbase/models_core/tests/test_mixable_cond_gaussian.py
+++ mb_modelbase/models_core/tests/test_mixable_cond_gaussian.py
@@ -1,5 +1,6 @@
 import unittest
 import numpy as np
+import pandas as pd
 
 from mb_modelbase.models_core.mixable_cond_gaussian import MixableCondGaussianModel as MixCondGauss
 
@@ -15,9 +16,31 @@ class TestMethods(unittest.TestCase):
         pass
 
 
-    def test_something(self):
+    def test_basics(self):
         self.assertEqual(self.model.name, 'TestMod')
+        self.assertEqual(len(self.model._sample(5)), 5)
 
+    def test_samplequality(self):
+
+        samples1 = self.model._sample(250)
+        data1 = pd.DataFrame(data=samples1, columns=self.model.names)
+        testmod1 = MixCondGauss("Allbus_test1"); testmod1.fit(df=data1)
+        error1 = ((np.array(testmod1._mu).ravel() - np.array(self.model._mu).ravel())**2).sum()
+
+        samples2 = self.model._sample(500)
+        data2 = pd.DataFrame(data=samples2, columns=self.model.names)
+        testmod2 = MixCondGauss("Allbus_test2"); testmod2.fit(df=data2)
+        error2 = ((np.array(testmod2._mu).ravel() - np.array(self.model._mu).ravel())**2).sum()
+
+        self.assertTrue(error2 < error1)
+
+    def test_onlycats(self):
+        margmod = self.model.copy().marginalize(keep=['sex'])
+        self.assertEqual(type(margmod._sample(1)[0][0]), type('str'))# == 'Male' or margmod._sample(1)[0][0] == 'Female')
+
+    def test_onlynumericals(self):
+        margmod = self.model.copy().marginalize(keep=['age'])
+        self.assertEqual(type(margmod._sample(1)[0][0]), type(0.12345))
 
 if __name__ == '__main__':
     unittest.main()
