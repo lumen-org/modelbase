0de3a050a69db69e759fe5e17b42a4b344e5960d
Christoph Saffer <christoph.saffer@uni-jena.de>
2018-05-22 12:54:22 +0200

    Implemented an example test case

--- mb_modelbase/models_core/tests/__init__.py
+++ mb_modelbase/models_core/tests/__init__.py
@@ -6,3 +6,4 @@ from mb_modelbase.models_core.tests.test_models import *
 from mb_modelbase.models_core.tests.test_models_generic import *
 from mb_modelbase.models_core.tests.test_crabs import *
 from mb_modelbase.models_core.tests.test_mixable_cond_gaussian import *
+from mb_modelbase.models_core.tests.test_allbus import *
--- mb_modelbase/models_core/tests/test_mixable_cond_gaussian.py
+++ mb_modelbase/models_core/tests/test_mixable_cond_gaussian.py
@@ -3,11 +3,20 @@ import numpy as np
 
 from mb_modelbase.models_core.mixable_cond_gaussian import MixableCondGaussianModel as MixCondGauss
 
+# load data
+from mb_modelbase.models_core.tests import test_allbus as ta
+
 class TestMethods(unittest.TestCase):
 
-    def test_something(self):
-        return 0
+    def setUp(self):
+        self.data = ta.mixed()
+        self.model = MixCondGauss("TestMod")
+        self.model.fit(df=self.data)
+        pass
 
+
+    def test_something(self):
+        self.assertEqual(self.model.name, 'TestMod')
 
 
 if __name__ == '__main__':
