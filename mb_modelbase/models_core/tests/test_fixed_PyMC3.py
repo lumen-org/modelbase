a5ad6ed803e0e69fcbd3f104101ae99aa91bcc68
Jonas Gütter <jonas.aaron.guetter@uni-jena.de>
2019-02-09 12:26:37 +0100

    Start using unittest for testing

--- mb_modelbase/models_core/tests/test_fixed_PyMC3.py
+++ mb_modelbase/models_core/tests/test_fixed_PyMC3.py
@@ -2,16 +2,8 @@ import numpy as np
 import pandas as pd
 import pymc3 as pm
 import mb_modelbase as mbase
+import unittest
 
-
-# # Generate data
-# np.random.seed(2)
-# size = 100
-# mu = np.random.normal(0, 1, size=size)
-# sigma = 1
-# X = np.random.normal(mu, sigma, size=size)
-# data = pd.DataFrame({'X': X})
-#
 # # Specify model
 # basic_model = pm.Model()
 # with basic_model:
@@ -19,26 +11,53 @@ import mb_modelbase as mbase
 #     mu = pm.Normal('mu', mu=0, sd=sigma)
 #     X = pm.Normal('X', mu=mu, sd=sigma, observed=data['X'])
 #
-#     nr_of_samples = 2000
-#     trace = pm.sample(nr_of_samples, tune=1000, cores=4)
-#
 # modelname = 'my_pymc3_model'
 # mymod = mbase.FixedProbabilisticModel(modelname, basic_model)
-# mymod.fit(data)
+
 #mymod = mbase.Model.load('/home/philipp/Documents/projects/graphical_models/code/mb_data/data_models/my_pymc3_model.mdl')
-mymod = mbase.Model.load('/home/guet_jn/Desktop/mb_data/data_models/my_pymc3_model.mdl')
-mymod.parallel_processing = False
+#mymod = mbase.Model.load('/home/guet_jn/Desktop/mb_data/data_models/my_pymc3_model.mdl')
 
 
-mymod_2 = mymod.copy()
-mymod_2 = mymod_2.condition([mbase.Condition("X", "<", -4)])
-mymod_2 = mymod_2.condition([mbase.Condition("X", ">", -6)])
-mymod_2.marginalize(remove=["X"])
-res = mymod_2.aggregate("maximum")
+class Testcopy(unittest.TestCase):
+    """
+    Test the FixedProbabilisticModel.copy() method
+    """
+    def test_copy_initialized_model(self):
+        """
+        Test the method on a model that was just initialized, and not changed in any other way yet
+        """
+        mymod = mbase.Model.load(
+            '/home/philipp/Documents/projects/graphical_models/code/mb_data/data_models/pymc3_testcase_model.mdl')
+        mymod_copy = mymod.copy()
+        self.assertEqual(mymod.data.equals(mymod_copy.data),1,"Copied model data is different than original model data")
+        self.assertEqual(mymod.test_data.equals(mymod_copy.test_data),1, "Copied model test data is different than original model test data")
+        self.assertEqual(mymod.samples.equals(mymod_copy.samples),1, "Copied samples are different than original samples")
+
+class Test_set_data(unittest.TestCase):
+    """
+    Test the FixedProbabilisticModel._set_data() method
+    """
+    def test_set_data(self):
+        """
+        Test if the method gives any data to the model and if the model data is the same as the input data
+        """
+        mymod = mbase.Model.load(
+            '/home/philipp/Documents/projects/graphical_models/code/mb_data/data_models/pymc3_testcase_model.mdl')
+        np.random.seed(2)
+        size = 100
+        mu = np.random.normal(0, 1, size=size)
+        sigma = 1
+        X = np.random.normal(mu, sigma, size=size)
+        data = pd.DataFrame({'X': X})
+        mymod.set_data(data)
+        self.assertEqual(mymod.data.empty,0, "There is no data in the model")
+        self.assertEqual(mymod.data.equals(data),1, "model data is different than the original data")
 
 
 
-#probabilitiy()
+if __name__ == "__main__":
+    unittest.main()
 
 
-print(res)
+
+
