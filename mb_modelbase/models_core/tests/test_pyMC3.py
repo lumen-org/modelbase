bacf59097a507a7af141225975accf2aee17a479
jong42 <jonasguetter@gmx.de>
2019-02-20 17:26:08 +0100

    Fix bug in test_conditionout
    The variables in samples do not have the same order as the variables in fields.
    Since this was not noticed before, I attempted to address the same variable in
    samples and in fields by using the same index. This index however addressed
    different variables, leading to the test failing, where it should pass. Variables
    are now addressed by name instead of index to avoid this.

--- mb_modelbase/models_core/tests/test_pyMC3.py
+++ mb_modelbase/models_core/tests/test_pyMC3.py
@@ -7,13 +7,15 @@ import unittest
 
 class Test_methods_on_initialized_model(unittest.TestCase):
     """
-    Test the FixedProbabilisticModel methods on a model that has just been initialized
+    Test the ProbabilisticPymc3Model methods on a model that has just been initialized
     """
+    testcasemodel_path = '/home/guet_jn/Desktop/mb_data/data_models/pymc3_testcase_model.mdl'
+
     def testinit(self):
         """
         Test if newly initialized model has data, test data or samples and if mode is set to None
         """
-        mymod = mbase.Model.load(testcasemodel_path)
+        mymod = mbase.Model.load(self.testcasemodel_path)
         self.assertEqual(mymod.data.empty,1,"There should be no data")
         self.assertEqual(mymod.test_data.empty, 1, "There should be no test data")
         self.assertEqual(mymod.samples.empty, 1, "There should be no samples")
@@ -23,7 +25,7 @@ class Test_methods_on_initialized_model(unittest.TestCase):
         """
         Test if data, test data and samples of the copied model are the same as in the original model
         """
-        mymod = mbase.Model.load(testcasemodel_path)
+        mymod = mbase.Model.load(self.testcasemodel_path)
         mymod_copy = mymod.copy()
         self.assertEqual(mymod.data.equals(mymod_copy.data),1,"Copied model data is different than original model data")
         self.assertEqual(mymod.test_data.equals(mymod_copy.test_data),1,
@@ -37,7 +39,7 @@ class Test_methods_on_initialized_model(unittest.TestCase):
         Test if the set_data() method gives any data to the model
         and if the model data has the same columns as the input data and if mode is set to data
         """
-        mymod = mbase.Model.load(testcasemodel_path)
+        mymod = mbase.Model.load(self.testcasemodel_path)
         np.random.seed(2)
         size = 100
         mu = np.random.normal(0, 1, size=size)
@@ -54,7 +56,7 @@ class Test_methods_on_initialized_model(unittest.TestCase):
     #     """
     #     Test if there are samples and test data in the model and if the mode is set to model
     #     """
-    #     mymod = mbase.Model.load(testcasemodel_path)
+    #     mymod = mbase.Model.load(self.testcasemodel_path)
     #     mymod.fit()
     #     self.assertEqual(mymod.samples.empty, 0, "There are no samples in the model")
     #     self.assertEqual(mymod.test_data.empty, 0, "There is no test data in the model")
@@ -65,7 +67,7 @@ class Test_methods_on_initialized_model(unittest.TestCase):
         Call _marginalizeout on a model without any samples.
         An error should be thrown since the model does not yet know any variables
         """
-        mymod = mbase.Model.load(testcasemodel_path)
+        mymod = mbase.Model.load(self.testcasemodel_path)
         with self.assertRaises(ValueError):
             mymod._marginalizeout(keep='A', remove='B')
         self.assertEqual(mymod.data.empty,1,"There should be no data")
@@ -78,7 +80,7 @@ class Test_methods_on_initialized_model(unittest.TestCase):
         Call _conditionout on a model without any samples.
         An error should be thrown since the model does not yet know any variables
         """
-        mymod = mbase.Model.load(testcasemodel_path)
+        mymod = mbase.Model.load(self.testcasemodel_path)
         with self.assertRaises(ValueError):
             mymod._conditionout(keep='A',remove='B')
         self.assertEqual(mymod.data.empty, 1, "There should be no data")
@@ -91,7 +93,7 @@ class Test_methods_on_initialized_model(unittest.TestCase):
         Calculate a probability density on a model.
         An error should be thrown since the model does not yet know any variables
         """
-        mymod = mbase.Model.load(testcasemodel_path)
+        mymod = mbase.Model.load(self.testcasemodel_path)
         with self.assertRaises(ValueError):
             mymod.density([0])
 
@@ -100,14 +102,15 @@ class Test_methods_on_initialized_model(unittest.TestCase):
         """
         Calculate the maximum probability of a model without samples. It should return an empty array
         """
-        mymod = mbase.Model.load(testcasemodel_path)
+        mymod = mbase.Model.load(self.testcasemodel_path)
         self.assertTrue(len(mymod._maximum())==0,
                         "maximum density point for a model without variables should be an empty array")
 
 class Test_methods_on_model_with_data(unittest.TestCase):
     """
-    Test the FixedProbabilisticModel methods on a model that has been initialized and given data
+    Test the ProbabilisticPymc3Model methods on a model that has been initialized and given data
     """
+    testcasemodel_path = '/home/guet_jn/Desktop/mb_data/data_models/pymc3_testcase_model.mdl'
     np.random.seed(2)
     size = 100
     mu = np.random.normal(0, 1, size=size)
@@ -119,7 +122,7 @@ class Test_methods_on_model_with_data(unittest.TestCase):
         """
         Test if data, test data and samples of the copied model are the same as in the original model
         """
-        mymod = mbase.Model.load(testcasemodel_path)
+        mymod = mbase.Model.load(self.testcasemodel_path)
         mymod.set_data(self.data)
         mymod_copy = mymod.copy()
         self.assertEqual(mymod.data.equals(mymod_copy.data),1,"Copied model data is different than original model data")
@@ -132,7 +135,7 @@ class Test_methods_on_model_with_data(unittest.TestCase):
         """
         Test if there are samples, data and test data in the model and if the mode is set to both
         """
-        mymod = mbase.Model.load(testcasemodel_path)
+        mymod = mbase.Model.load(self.testcasemodel_path)
         mymod.fit(self.data)
         self.assertEqual(mymod.data.empty,0, "There is no data in the model")
         self.assertEqual(mymod.test_data.empty, 0, "There is no test data in the model")
@@ -144,7 +147,7 @@ class Test_methods_on_model_with_data(unittest.TestCase):
         Call _marginalizeout on a model without any samples for variables not in the model.
         An error should be thrown since the model does not have the variables
         """
-        mymod = mbase.Model.load(testcasemodel_path)
+        mymod = mbase.Model.load(self.testcasemodel_path)
         mymod.set_data(self.data)
         with self.assertRaises(ValueError):
             mymod._marginalizeout(keep='A', remove='B')
@@ -157,7 +160,7 @@ class Test_methods_on_model_with_data(unittest.TestCase):
         Call _conditionout on a model without any samples for variables not in the model.
         An error should be thrown since the model does not have the variables
         """
-        mymod = mbase.Model.load(testcasemodel_path)
+        mymod = mbase.Model.load(self.testcasemodel_path)
         mymod.set_data(self.data)
         with self.assertRaises(ValueError):
             mymod._conditionout(keep='A',remove='B')
@@ -170,7 +173,7 @@ class Test_methods_on_model_with_data(unittest.TestCase):
         Calculate a probability density on a model.
         An error should be thrown since the model does not yet know any variables
         """
-        mymod = mbase.Model.load(testcasemodel_path)
+        mymod = mbase.Model.load(self.testcasemodel_path)
         mymod.set_data(self.data)
         with self.assertRaises(ValueError):
             mymod.density([0])
@@ -179,15 +182,16 @@ class Test_methods_on_model_with_data(unittest.TestCase):
         """
         Calculate the maximum probability of a model without samples. It should return an empty array
         """
-        mymod = mbase.Model.load(testcasemodel_path)
+        mymod = mbase.Model.load(self.testcasemodel_path)
         mymod.set_data(self.data)
         self.assertTrue(len(mymod._maximum())==0,
                         "maximum density point for a model without samples should be an empty array")
 
 class Test_methods_on_fitted_model(unittest.TestCase):
     """
-    Test the FixedProbabilisticModel methods on a model that has been initialized, given data and fitted
+    Test the ProbabilisticPymc3Model methods on a model that has been initialized, given data and fitted
     """
+    testcasemodel_path = '/home/guet_jn/Desktop/mb_data/data_models/pymc3_testcase_model.mdl'
     np.random.seed(2)
     size = 100
     mu = np.random.normal(0, 1, size=size)
@@ -199,7 +203,7 @@ class Test_methods_on_fitted_model(unittest.TestCase):
         """
         Test if data, test data and samples of the copied model are the same as in the original model
         """
-        mymod = mbase.Model.load(testcasemodel_path)
+        mymod = mbase.Model.load(self.testcasemodel_path)
         mymod.fit(self.data)
         mymod_copy = mymod.copy()
         self.assertEqual(mymod.data.equals(mymod_copy.data),1,"Copied model data is different than original model data")
@@ -212,7 +216,7 @@ class Test_methods_on_fitted_model(unittest.TestCase):
         """
         Call _marginalizeout on a fitted model. Check if the correct variables are removed from the model
         """
-        mymod = mbase.Model.load(testcasemodel_path)
+        mymod = mbase.Model.load(self.testcasemodel_path)
         mymod.fit(self.data)
         keep = mymod.names[1:]
         remove = mymod.names[0]
@@ -231,15 +235,15 @@ class Test_methods_on_fitted_model(unittest.TestCase):
         Call _conditionout on a fitted model. Check if the correct variables are removed from the model
         and if all the samples are within the variable domain
         """
-        mymod = mbase.Model.load(testcasemodel_path)
+        mymod = mbase.Model.load(self.testcasemodel_path)
         mymod.fit(self.data)
         keep = mymod.names[1:]
         remove = [mymod.names[0]]
 
         sample_size_all_values = len(mymod.samples)
-        mymod.fields[0]['domain'].setupperbound(np.mean(mymod.samples.iloc[:, 0]))
-        isBiggerThanUpperBound = mymod.samples.iloc[:,0] >= np.mean(mymod.samples.iloc[:, 0])
-        big_samples = mymod.samples.iloc[:, 0] [isBiggerThanUpperBound]
+        mymod.fields[0]['domain'].setupperbound(np.mean(mymod.samples[remove[0]]))
+        isBiggerThanUpperBound = mymod.samples[remove[0]] > np.mean(mymod.samples[remove[0]])
+        big_samples = mymod.samples[remove[0]] [isBiggerThanUpperBound]
         sample_size_big_values = len(big_samples)
 
         mymod._conditionout(keep= keep, remove=remove)
@@ -259,7 +263,7 @@ class Test_methods_on_fitted_model(unittest.TestCase):
         """
         Calculate a probability density on a model. A single scalar should be the return value
         """
-        mymod = mbase.Model.load(testcasemodel_path)
+        mymod = mbase.Model.load(self.testcasemodel_path)
         mymod.fit(self.data)
         location = np.zeros(len(mymod.names))
         self.assertTrue(isinstance(mymod.density(location),float),"A single scalar should be returned")
@@ -268,7 +272,7 @@ class Test_methods_on_fitted_model(unittest.TestCase):
         """
         Calculate the maximum probability of a model. Dimensions should match
         """
-        mymod = mbase.Model.load(testcasemodel_path)
+        mymod = mbase.Model.load(self.testcasemodel_path)
         mymod.fit(self.data)
         self.assertEqual(len(mymod._maximum()),len(mymod.names),"Dimension of the maximum does not match dimension of the model")
 
@@ -276,6 +280,7 @@ class Test_more_combinations_on_model(unittest.TestCase):
     """
     Test more complex cases, with more combinations of methods being applied to a already fitted model
     """
+    testcasemodel_path = '/home/guet_jn/Desktop/mb_data/data_models/pymc3_testcase_model.mdl'
     np.random.seed(2)
     size = 100
     mu = np.random.normal(0, 1, size=size)
@@ -290,7 +295,7 @@ class Test_more_combinations_on_model(unittest.TestCase):
         """
         Check if the density maximum of a marginalized model has the same dimensions as the model variables
         """
-        mymod = mbase.Model.load(testcasemodel_path)
+        mymod = mbase.Model.load(self.testcasemodel_path)
         mymod.fit(self.data)
         remove = mymod.names[0]
         mymod.marginalize(remove=remove)
@@ -299,7 +304,7 @@ class Test_more_combinations_on_model(unittest.TestCase):
 if __name__ == "__main__":
 
     # testcasemodel_path = '/home/philipp/Documents/projects/graphical_models/code/mb_data/data_models/pymc3_testcase_model.mdl'
-    testcasemodel_path = '/home/guet_jn/Desktop/mb_data/data_models/pymc3_testcase_model.mdl'
+    #testcasemodel_path = '/home/guet_jn/Desktop/mb_data/data_models/pymc3_testcase_model.mdl'
     unittest.main()
 
 
