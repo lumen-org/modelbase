4113c257ee61f70cd1f127163bd4b5591d2b91eb
jong42 <jonasguetter@gmx.de>
2019-04-09 13:55:04 +0200

    Add testcase for independent variables

--- mb_modelbase/models_core/tests/create_PyMC3_testmodels.py
+++ mb_modelbase/models_core/tests/create_PyMC3_testmodels.py
@@ -6,8 +6,7 @@ from mb_modelbase.models_core.pyMC3_model import ProbabilisticPymc3Model
 import matplotlib.pyplot as plt
 from pylab import hist
 
-# testcasemodel_path = '/home/guet_jn/Desktop/mb_data/data_models/'
-testcasemodel_path = '/home/philipp/Documents/projects/graphical_models/code/mb_data/data_models/'
+testcasemodel_path = '/home/guet_jn/Desktop/mb_data/data_models/'
 
 ######################################
 # pymc3_testcase_model
@@ -36,7 +35,44 @@ testcasemodel_path = '/home/philipp/Documents/projects/graphical_models/code/mb_
 # pymc3_getting_started_model
 ######################################
 
-modelname = 'pymc3_getting_started_model_fitted'
+# modelname = 'pymc3_getting_started_model_fitted'
+# np.random.seed(123)
+# alpha, sigma = 1, 1
+# beta_0 = 1
+# beta_1 = 2.5
+# size = 100
+# X1 = np.random.randn(size)
+# X2 = np.random.randn(size) * 0.2
+# Y = alpha + beta_0 * X1 + beta_1 * X2 + np.random.randn(size) * sigma
+# data = pd.DataFrame({'X1': X1, 'X2': X2, 'Y': Y})
+#
+#
+# basic_model = pm.Model()
+#
+# with basic_model:
+#     # Priors for unknown model parameters
+#     alpha = pm.Normal('alpha', mu=0, sd=10)
+#     beta_0 = pm.Normal('beta_0', mu=0, sd=10)
+#     beta_1 = pm.Normal('beta_1', mu=0, sd=10)
+#     sigma = pm.HalfNormal('sigma', sd=1)
+#
+#     # Expected value of outcome
+#     mu = alpha + beta_0 * data['X1'] + beta_1 * data['X2']
+#
+#     # Likelihood (sampling distribution) of observations
+#     Y = pm.Normal('Y', mu=mu, sd=sigma, observed=data['Y'])
+#     X1 = pm.Normal('X1', mu=data['X1'], sd=sigma, observed=data['X1'])
+#     X2 = pm.Normal('X2', mu=data['X2'], sd=sigma, observed=data['X2'])
+#
+# m = ProbabilisticPymc3Model(modelname, basic_model)
+# m.fit(data)
+# Model.save(m, testcasemodel_path + modelname + '.mdl')
+
+###############################################
+# pymc3_getting_started_model_independent vars
+###############################################
+
+modelname = 'pymc3_getting_started_model_independent_vars_fitted'
 np.random.seed(123)
 alpha, sigma = 1, 1
 beta_0 = 1
@@ -46,7 +82,6 @@ X1 = np.random.randn(size)
 X2 = np.random.randn(size) * 0.2
 Y = alpha + beta_0 * X1 + beta_1 * X2 + np.random.randn(size) * sigma
 data = pd.DataFrame({'X1': X1, 'X2': X2, 'Y': Y})
-
 
 basic_model = pm.Model()
 
@@ -62,13 +97,10 @@ with basic_model:
 
     # Likelihood (sampling distribution) of observations
     Y = pm.Normal('Y', mu=mu, sd=sigma, observed=data['Y'])
-    X1 = pm.Normal('X1', mu=data['X1'], sd=sigma, observed=data['X1'])
-    X2 = pm.Normal('X2', mu=data['X2'], sd=sigma, observed=data['X2'])
 
 m = ProbabilisticPymc3Model(modelname, basic_model)
 m.fit(data)
 Model.save(m, testcasemodel_path + modelname + '.mdl')
-
 ######################################
 # pymc3_coal_mining_disaster_model
 ######################################
