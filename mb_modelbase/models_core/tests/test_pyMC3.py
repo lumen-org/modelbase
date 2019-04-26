944eb564126d6859ce07c8669b32f3a1f8989d9c
jong42 <jonasguetter@gmx.de>
2019-04-26 16:45:35 +0200

    Add ,odel name to error messages of tests

--- mb_modelbase/models_core/tests/test_pyMC3.py
+++ mb_modelbase/models_core/tests/test_pyMC3.py
@@ -27,10 +27,10 @@ class Test_methods_on_initialized_model(unittest.TestCase):
         """
         for i, name in enumerate(model_paths):
             mymod = mbase.Model.load(model_paths[i])
-            self.assertEqual(mymod.data.empty,1,"There should be no data")
-            self.assertEqual(mymod.test_data.empty, 1, "There should be no test data")
-            self.assertEqual(mymod.samples.empty, 1, "There should be no samples")
-            self.assertIsNone(mymod.mode, "Mode of just instantiated model should be set to None")
+            self.assertEqual(mymod.data.empty, 1, "There should be no data. Model:" + mymod.name)
+            self.assertEqual(mymod.test_data.empty, 1, "There should be no test data. Model:" + mymod.name)
+            self.assertEqual(mymod.samples.empty, 1, "There should be no samples. Model:" + mymod.name)
+            self.assertIsNone(mymod.mode, "Mode of just instantiated model should be set to None. Model:" + mymod.name)
 
     def testcopy(self):
         """
@@ -39,11 +39,12 @@ class Test_methods_on_initialized_model(unittest.TestCase):
         for i, name in enumerate(model_paths):
             mymod = mbase.Model.load(model_paths[i])
             mymod_copy = mymod.copy()
-            self.assertEqual(mymod.data.equals(mymod_copy.data),1,"Copied model data is different than original model data")
-            self.assertEqual(mymod.test_data.equals(mymod_copy.test_data),1,
-                             "Copied model test data is different than original model test data")
-            self.assertEqual(mymod.samples.equals(mymod_copy.samples),1,
-                             "Copied samples are different than original samples")
+            self.assertEqual(mymod.data.equals(mymod_copy.data), 1,
+                             "Copied model data is different than original model data. Model:" + mymod.name)
+            self.assertEqual(mymod.test_data.equals(mymod_copy.test_data), 1,
+                             "Copied model test data is different than original model test data. Model: " + mymod.name)
+            self.assertEqual(mymod.samples.equals(mymod_copy.samples), 1,
+                             "Copied samples are different than original samples. Model: " + mymod.name)
 
     def test_set_data(self):
         """
@@ -54,9 +55,10 @@ class Test_methods_on_initialized_model(unittest.TestCase):
             mymod = mbase.Model.load(model_paths[i])
             data = pd.read_csv(data_paths[i])
             mymod.set_data(data)
-            self.assertEqual(mymod.data.empty,0, "There is no data in the model")
-            self.assertEqual(mymod.data.columns.equals(data.columns),1, "model data has different columns than the original data")
-            self.assertEqual(mymod.mode,'data', "model mode should be set to data")
+            self.assertEqual(mymod.data.empty, 0, "There is no data in the model. Model: " + mymod.name)
+            self.assertEqual(mymod.data.columns.equals(data.columns), 1,
+                             "model data has different columns than the original data. Model: " + mymod.name)
+            self.assertEqual(mymod.mode, 'data', "model mode should be set to data. Model: " + mymod.name)
 
     # TODO: What should happen, if the fit method is called on a model without data?
     # def test_fit(self):
@@ -78,10 +80,10 @@ class Test_methods_on_initialized_model(unittest.TestCase):
             mymod = mbase.Model.load(model_paths[i])
             with self.assertRaises(ValueError):
                 mymod._marginalizeout(keep='A', remove='B')
-            self.assertEqual(mymod.data.empty,1,"There should be no data")
-            self.assertEqual(mymod.test_data.empty, 1, "There should be no test data")
-            self.assertEqual(mymod.samples.empty, 1, "There should be no samples")
-            self.assertIsNone(mymod.mode, "Mode of just instantiated model should be set to None")
+            self.assertEqual(mymod.data.empty, 1, "There should be no data. Model: " + mymod.name)
+            self.assertEqual(mymod.test_data.empty, 1, "There should be no test data. Model: " + mymod.name)
+            self.assertEqual(mymod.samples.empty, 1, "There should be no samples. Model: " + mymod.name)
+            self.assertIsNone(mymod.mode, "Mode of just instantiated model should be set to None. Model: " + mymod.name)
 
     def test_conditionout(self):
         """
@@ -91,11 +93,11 @@ class Test_methods_on_initialized_model(unittest.TestCase):
         for i, name in enumerate(model_paths):
             mymod = mbase.Model.load(model_paths[i])
             with self.assertRaises(ValueError):
-                mymod._conditionout(keep='A',remove='B')
-            self.assertEqual(mymod.data.empty, 1, "There should be no data")
-            self.assertEqual(mymod.test_data.empty, 1, "There should be no test data")
-            self.assertEqual(mymod.samples.empty, 1, "There should be no samples")
-            self.assertIsNone(mymod.mode, "Mode of just instantiated model should be set to None")
+                mymod._conditionout(keep='A', remove='B')
+            self.assertEqual(mymod.data.empty, 1, "There should be no data. Model: " + mymod.name)
+            self.assertEqual(mymod.test_data.empty, 1, "There should be no test data. Model: " + mymod.name)
+            self.assertEqual(mymod.samples.empty, 1, "There should be no samples. Model: " + mymod.name)
+            self.assertIsNone(mymod.mode, "Mode of just instantiated model should be set to None. Model: " + mymod.name)
 
     def test_density(self):
         """
@@ -113,8 +115,9 @@ class Test_methods_on_initialized_model(unittest.TestCase):
         """
         for i, name in enumerate(model_paths):
             mymod = mbase.Model.load(model_paths[i])
-            self.assertTrue(len(mymod._maximum())==0,
-                            "maximum density point for a model without variables should be an empty array")
+            self.assertTrue(len(mymod._maximum()) == 0,
+                            "maximum density point for a model without variables should be an empty array. "
+                            "Model: " + mymod.name)
 
 class Test_methods_on_model_with_data(unittest.TestCase):
     """
@@ -130,11 +133,12 @@ class Test_methods_on_model_with_data(unittest.TestCase):
             data = pd.read_csv(data_paths[i])
             mymod.set_data(data)
             mymod_copy = mymod.copy()
-            self.assertEqual(mymod.data.equals(mymod_copy.data),1,"Copied model data is different than original model data")
-            self.assertEqual(mymod.test_data.equals(mymod_copy.test_data),1,
-                             "Copied model test data is different than original model test data")
-            self.assertEqual(mymod.samples.equals(mymod_copy.samples),1,
-                             "Copied samples are different than original samples")
+            self.assertEqual(mymod.data.equals(mymod_copy.data), 1,
+                             "Copied model data is different than original model data. Model: " + mymod.name)
+            self.assertEqual(mymod.test_data.equals(mymod_copy.test_data), 1,
+                             "Copied model test data is different than original model test data. Model: " + mymod.name)
+            self.assertEqual(mymod.samples.equals(mymod_copy.samples), 1,
+                             "Copied samples are different than original samples. Model: " + mymod.name)
 
     def test_fit(self):
         """
@@ -144,12 +148,14 @@ class Test_methods_on_model_with_data(unittest.TestCase):
             mymod = mbase.Model.load(model_paths[i])
             data = pd.read_csv(data_paths[i])
             mymod.fit(data)
-            self.assertEqual(mymod.data.empty,0, "There is no data in the model")
-            self.assertEqual(mymod.test_data.empty, 0, "There is no test data in the model")
-            self.assertEqual(mymod.samples.empty, 0, "There are no samples in the model")
-            self.assertEqual(mymod.mode, 'both', "mode should be set to both")
-            self.assertEqual(mymod.names,list(mymod.samples.columns.values), "names and samples should hold the same variables in the same order")
-            self.assertEqual(mymod.names,[field['name'] for field in mymod.fields], "names and fields should hold the same variables in the same order")
+            self.assertEqual(mymod.data.empty, 0, "There is no data in the model. Model: " + mymod.name)
+            self.assertEqual(mymod.test_data.empty, 0, "There is no test data in the model. Model: " + mymod.name)
+            self.assertEqual(mymod.samples.empty, 0, "There are no samples in the model. Model: " + mymod.name)
+            self.assertEqual(mymod.mode, 'both', "mode should be set to both. Model: " + mymod.name)
+            self.assertEqual(mymod.names, list(mymod.samples.columns.values),
+                             "names and samples should hold the same variables in the same order. Model: " + mymod.name)
+            self.assertEqual(mymod.names, [field['name'] for field in mymod.fields],
+                             "names and fields should hold the same variables in the same order. Model: " + mymod.name)
 
     def test_marginalizeout(self):
         """
@@ -162,9 +168,10 @@ class Test_methods_on_model_with_data(unittest.TestCase):
             mymod.set_data(data)
             with self.assertRaises(ValueError):
                 mymod._marginalizeout(keep='A', remove='B')
-            self.assertEqual(mymod.data.empty,0,"There should be data")
-            self.assertEqual(mymod.samples.empty, 1, "There should be no samples")
-            self.assertEqual(mymod.mode,"data", "Mode of just instantiated model should be set to data")
+            self.assertEqual(mymod.data.empty, 0, "There should be data. Model: " + mymod.name)
+            self.assertEqual(mymod.samples.empty, 1, "There should be no samples. Model: " + mymod.name)
+            self.assertEqual(mymod.mode, "data",
+                             "Mode of just instantiated model should be set to data. Model: " + mymod.name)
 
     def test_conditionout(self):
         """
@@ -176,10 +183,11 @@ class Test_methods_on_model_with_data(unittest.TestCase):
             data = pd.read_csv(data_paths[i])
             mymod.set_data(data)
             with self.assertRaises(ValueError):
-                mymod._conditionout(keep='A',remove='B')
-            self.assertEqual(mymod.data.empty, 0, "There should be data")
-            self.assertEqual(mymod.samples.empty, 1, "There should be no samples")
-            self.assertEqual(mymod.mode,"data", "Mode of just instantiated model should be set to data")
+                mymod._conditionout(keep='A', remove='B')
+            self.assertEqual(mymod.data.empty, 0, "There should be data. Model: " + mymod.name)
+            self.assertEqual(mymod.samples.empty, 1, "There should be no samples. Model: " + mymod.name)
+            self.assertEqual(mymod.mode, "data",
+                             "Mode of just instantiated model should be set to data. Model: " + mymod.name)
 
     def test_density(self):
         """
@@ -201,8 +209,9 @@ class Test_methods_on_model_with_data(unittest.TestCase):
             mymod = mbase.Model.load(model_paths[i])
             data = pd.read_csv(data_paths[i])
             mymod.set_data(data)
-            self.assertTrue(len(mymod._maximum())==0,
-                            "maximum density point for a model without samples should be an empty array")
+            self.assertTrue(len(mymod._maximum()) == 0,
+                            "maximum density point for a model without samples should be an empty array. "
+                            "Model: " + mymod.name)
 
 class Test_methods_on_fitted_model(unittest.TestCase):
     """
@@ -218,11 +227,12 @@ class Test_methods_on_fitted_model(unittest.TestCase):
             data = pd.read_csv(data_paths[i])
             mymod.fit(data)
             mymod_copy = mymod.copy()
-            self.assertEqual(mymod.data.equals(mymod_copy.data),1,"Copied model data is different than original model data")
-            self.assertEqual(mymod.test_data.equals(mymod_copy.test_data),1,
-                             "Copied model test data is different than original model test data")
-            self.assertEqual(mymod.samples.equals(mymod_copy.samples),1,
-                             "Copied samples are different than original samples")
+            self.assertEqual(mymod.data.equals(mymod_copy.data), 1,
+                             "Copied model data is different than original model data. Model: " + mymod.name)
+            self.assertEqual(mymod.test_data.equals(mymod_copy.test_data), 1,
+                             "Copied model test data is different than original model test data. Model: " + mymod.name)
+            self.assertEqual(mymod.samples.equals(mymod_copy.samples), 1,
+                             "Copied samples are different than original samples. Model: " + mymod.name)
 
     def test_marginalizeout(self):
         """
@@ -234,15 +244,17 @@ class Test_methods_on_fitted_model(unittest.TestCase):
             mymod.fit(data)
             keep = mymod.names[1:]
             remove = [mymod.names[0]]
-            mymod._marginalizeout(keep= keep, remove=remove)
-            self.assertEqual(mymod.data.empty,0,"There should be data")
-            self.assertEqual(mymod.test_data.empty, 0, "There should be test data")
-            self.assertEqual(mymod.samples.empty, 0, "There should be samples")
-            self.assertEqual(mymod.mode,"both", "Mode of just instantiated model should be set to both")
+            mymod._marginalizeout(keep=keep, remove=remove)
+            self.assertEqual(mymod.data.empty, 0,"There should be data. Model: " + mymod.name)
+            self.assertEqual(mymod.test_data.empty, 0, "There should be test data. Model: " + mymod.name)
+            self.assertEqual(mymod.samples.empty, 0, "There should be samples. Model: " + mymod.name)
+            self.assertEqual(mymod.mode, "both",
+                             "Mode of just instantiated model should be set to both. Model: " + mymod.name)
             self.assertFalse(remove[0] in mymod.samples.columns,
-                             str(remove) + " should be marginalized out and not be present in the samples")
+                             str(remove) + " should be marginalized out and not be present in the samples. "
+                                           "Model: " + mymod.name)
             self.assertTrue(all([name in mymod.samples.columns for name in keep]),
-                            str(keep) + "should be still present in the samples")
+                            str(keep) + "should be still present in the samples. Model: " + mymod.name)
 
     def test_conditionout(self):
         """
@@ -263,17 +275,20 @@ class Test_methods_on_fitted_model(unittest.TestCase):
             sample_size_big_values = len(big_samples)
 
             mymod._conditionout(keep= keep, remove=remove)
-            self.assertEqual(mymod.data.empty,0,"There should be data")
-            self.assertEqual(mymod.test_data.empty, 0, "There should be test data")
-            self.assertEqual(mymod.samples.empty, 0, "There should be samples")
-            self.assertEqual(mymod.mode,"both", "Mode of just instantiated model should be set to both")
+            self.assertEqual(mymod.data.empty, 0, "There should be data. Model: " + mymod.name)
+            self.assertEqual(mymod.test_data.empty, 0, "There should be test data. Model: " + mymod.name)
+            self.assertEqual(mymod.samples.empty, 0, "There should be samples. Model: " + mymod.name)
+            self.assertEqual(mymod.mode,"both",
+                             "Mode of just instantiated model should be set to both. Model: " + mymod.name)
             self.assertFalse(remove in mymod.samples.columns.values,
-                             str(remove) + " should be marginalized out and not be present in the samples")
+                             str(remove) + " should be marginalized out and not be present in the samples. "
+                                           "Model: " + mymod.name)
             self.assertTrue(all([name in mymod.samples.columns for name in keep]),
-                            str(keep) + "should be still present in the samples")
+                            str(keep) + "should be still present in the samples. Model: " + mymod.name)
             sample_size_small_values = len(mymod.samples)
-            self.assertEqual(sample_size_all_values-sample_size_big_values,sample_size_small_values,
-                             "numbers of removed samples and kept samples do not add up to previous number of samples")
+            self.assertEqual(sample_size_all_values-sample_size_big_values, sample_size_small_values,
+                             "numbers of removed samples and kept samples do not add up to previous number of samples. "
+                             "Model: " + mymod.name)
 
     def test_density(self):
         """
@@ -284,7 +299,8 @@ class Test_methods_on_fitted_model(unittest.TestCase):
             data = pd.read_csv(data_paths[i])
             mymod.fit(data)
             location = np.zeros(len(mymod.names))
-            self.assertTrue(isinstance(mymod.density(location),float),"A single scalar should be returned")
+            self.assertTrue(isinstance(mymod.density(location), float),
+                            "A single scalar should be returned. Model: " + mymod.name)
 
     def test_maximum(self):
         """
@@ -294,14 +310,16 @@ class Test_methods_on_fitted_model(unittest.TestCase):
             mymod = mbase.Model.load(model_paths[i])
             data = pd.read_csv(data_paths[i])
             mymod.fit(data)
-            self.assertEqual(len(mymod._maximum()),len(mymod.names),"Dimension of the maximum does not match dimension of the model")
+            self.assertEqual(len(mymod._maximum()), len(mymod.names),
+                             "Dimension of the maximum does not match dimension of the model. Model: " + mymod.name)
 
 class Test_more_combinations_on_model(unittest.TestCase):
     """
     Test more complex cases, with more combinations of methods being applied to a already fitted model
     """
 
-    # More combinations of marginalization and conditionalization cannot be applid to the simple model since it only has two variables
+    # More combinations of marginalization and conditionalization cannot be applied
+    # to the simple model since it only has two variables
     # TODO: What happens when each variable is marginalized out?
 
     def test_maximum_marginalized(self):
@@ -314,7 +332,9 @@ class Test_more_combinations_on_model(unittest.TestCase):
             mymod.fit(data)
             remove = mymod.names[0]
             mymod.marginalize(remove=remove)
-            self.assertEqual(len(mymod._maximum()),len(mymod.names),"Dimensions of the maximum and the model variables do not match")
+            self.assertEqual(len(mymod._maximum()), len(mymod.names),
+                             "Dimensions of the maximum and the model variables do not match. Model: " + mymod.name)
+
 
 if __name__ == "__main__":
 
