cc376865e69e4e1dd7c2a5e3074b561f94786e74
jong42 <jonasguetter@gmx.de>
2019-05-21 10:00:46 +0200

    Work on kde tests

--- mb_modelbase/models_core/tests/test_kde_models.py
+++ mb_modelbase/models_core/tests/test_kde_models.py
@@ -10,7 +10,7 @@ class kde_test(unittest.TestCase):
     """
     def test_fit_onedim(self):
         data = pd.DataFrame({'X': np.array([1, 2, 3, 3, 3, 4, 5])})
-        kde_model = KDEModel()
+        kde_model = KDEModel('kde_model')
         self.assertIsNone(kde_model.kde, "Before fitting there should be no kde object")
         kde_model.set_data(data)
         kde_model.fit()
@@ -20,7 +20,7 @@ class kde_test(unittest.TestCase):
         data = pd.DataFrame({'A': np.array([1, 2, 3, 3, 3, 4, 5]), 'B': np.array([1, 1, 1, 3, 3, 4, 5]),
                              'C': np.array([1, 2, 2, 2, 2, 2, 5]), 'D': np.array([2, 2, 3, 3, 3, 2, 5]),
                              'E': np.array([1, 5, 3, 3, 3, 5, 5]), 'F': np.array([1, 8, 3, 3, 5, 1, 5])})
-        kde_model = KDEModel()
+        kde_model = KDEModel('kde_model')
         self.assertIsNone(kde_model.kde, "Before fitting there should be no kde object")
         kde_model.set_data(data)
         kde_model.fit()
@@ -29,7 +29,7 @@ class kde_test(unittest.TestCase):
     # what should happen for categoricals?
     def test_fit_categoricals(self):
         data = pd.DataFrame({'A': np.array([1, 2, 3, 3, 3, 4, 5]), 'B': np.array(['1', '2', '3', '3', '3', '4', '5'])})
-        kde_model = KDEModel()
+        kde_model = KDEModel('kde_model')
         self.assertIsNone(kde_model.kde, "Before fitting there should be no kde object")
         kde_model.set_data(data)
         kde_model.fit()
