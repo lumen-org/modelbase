4fd3c09bc1428ff3e51e3e40ab098bb464e35307
jong42 <jonasguetter@gmx.de>
2019-05-22 17:14:32 +0200

    COntinue tests

--- mb_modelbase/models_core/tests/test_kde_models.py
+++ mb_modelbase/models_core/tests/test_kde_models.py
@@ -35,11 +35,13 @@ class kde_test(unittest.TestCase):
         self.assertIsNotNone(kde_model.kde, "After fitting there should be a kde object")
 
     def test_conditionout(self):
-        data = pd.DataFrame({'A': np.array([1, 2, 3, 3, 3, 4, 5]), 'B': np.array(['1', '2', '3', '3', '3', '4', '5'])})
+        data = pd.DataFrame({'B': np.array(['1', '2', '3', '3', '3', '4', '5']), 'A': np.array([1, 2, 3, 3, 3, 4, 5])},
+                            columns=['B', 'A'])
         kde_model = KDEModel('kde_model')
-        kde_model.fit(data)
-        model_data = pd.concat([kde_model.data, kde_model.test_data]).sort_index()
-        self.assertEqual(model_data, data, "input data was not passed properly to the model")
+        kde_model._set_data_mixed(data, silently_drop=False, split_data=False)
+        kde_model.fit()
+        #model_data = pd.concat([kde_model.data, kde_model.test_data]).sort_index()
+        self.assertTrue(kde_model.data.equals(data), "input data was not passed properly to the model")
         # Change domains of dimension A
         kde_model.fields[1]['domain'].setlowerbound(2)
         kde_model.fields[1]['domain'].setupperbound(4)
@@ -50,5 +52,6 @@ class kde_test(unittest.TestCase):
         condition_lower = data['A'] >= 2
         data_cond = data[condition_upper & condition_lower]
         data_cond = data_cond['B']
-        self.assertEqual(data_cond, kde_model.data, "model data was not marginalized and conditioned properly")
+        self.assertTrue(kde_model.data.equals(data_cond), "model data was not marginalized and conditioned properly")
 
+# TODO: Also write a test for discrete domains
