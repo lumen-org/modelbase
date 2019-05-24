8f8d6cd1ab72b2fcecdb52a1bd2d05a8dfecf377
jong42 <jonasguetter@gmx.de>
2019-05-24 11:52:43 +0200

    Change _conditionout method
    The previous implementation of the method did not work since it
    required the data of the dimensions in remove. This data is
    however marginalized out before by the _marginalize() method.
    As a workaround I recreated the approach from the EmpiricalModel
    class, where there is an additional data object that stores
    the same data as self.data during the fitting process. This
    object is then not touched by _marginalize() and can be
    conditioned on later. At the end of the _conditionout() method,
    only those indexes in self.data are kept that appear also in
    the additional data object

--- mb_modelbase/models_core/kde_model.py
+++ mb_modelbase/models_core/kde_model.py
@@ -20,6 +20,7 @@ class KDEModel(Model):
     def __init__(self, name):
         super().__init__(name)
         self.kde = None
+        self._emp_data = None
 
     def _set_data(self, df, drop_silently, **kwargs):
         self._set_data_mixed(df, drop_silently)
@@ -27,24 +28,44 @@ class KDEModel(Model):
 
     def _fit(self):
         self.kde = KernelDensity(kernel='gaussian', bandwidth=0.1).fit(self.data.values)
+        # This is necessary for conditioning on the data later
+        self._emp_data = self.data.copy()
         return()
 
     def _conditionout(self, keep, remove):
         """Conditions the random variables with name in remove on their domain and marginalizes them out.
         """
+        # for name in remove:
+        #     # Remove all rows from the data that are outside the domain in name
+        #     lower_bound = self.byname(name)['domain'].values()[0]
+        #     upper_bound = self.byname(name)['domain'].values()[1]
+        #     lower_cond = self.data[name] >= lower_bound
+        #     upper_cond = self.data[name] <= upper_bound
+        #     self.data = self.data[lower_cond & upper_cond]
+        #     # Do the same for the test data
+        #     lower_cond = self.test_data[name] >= lower_bound
+        #     upper_cond = self.test_data[name] <= upper_bound
+        #     self.test_data = self.test_data[lower_cond & upper_cond]
+        # self.data = self.data.drop(remove, axis=1)
+        # self.test_data = self.test_data.drop(remove, axis=1)
+
+        # collect conditions
+        #values = [self.byname(r)['domain'].values() for r in remove]
+        #conditions = zip(remove, ['in']*len(values), values)
+        # condition
+        #self._emp_data = data_op.condition_data(self._emp_data, conditions)
+
+
         for name in remove:
-            # Remove all rows from the data that are outside the domain in name
+            # Remove all rows from the _emp_data that are outside the domain in name
             lower_bound = self.byname(name)['domain'].values()[0]
             upper_bound = self.byname(name)['domain'].values()[1]
-            lower_cond = self.data[name] >= lower_bound
-            upper_cond = self.data[name] <= upper_bound
-            self.data = self.data[lower_cond & upper_cond]
-            # Do the same for the test data
-            lower_cond = self.test_data[name] >= lower_bound
-            upper_cond = self.test_data[name] <= upper_bound
-            self.test_data = self.test_data[lower_cond & upper_cond]
-        self.data = self.data.drop(remove, axis=1)
-        self.test_data = self.test_data.drop(remove, axis=1)
+            lower_cond = self._emp_data[name] >= lower_bound
+            upper_cond = self._emp_data[name] <= upper_bound
+            self._emp_data = self._emp_data[lower_cond & upper_cond]
+
+        # transfer condition from _emp_data to data
+        self.data = self.data.loc[list(self._emp_data.index.values), :]
         self._marginalizeout(keep, remove)
 
     def _marginalizeout(self, keep, remove):
