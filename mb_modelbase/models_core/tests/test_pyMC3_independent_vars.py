1115705e6ff7c76a346ee8fb6b0e841a83f1e8dc
jong42 <jonasguetter@gmx.de>
2019-07-03 10:53:55 +0200

    Minor change

--- mb_modelbase/models_core/tests/test_pyMC3_independent_vars.py
+++ mb_modelbase/models_core/tests/test_pyMC3_independent_vars.py
@@ -45,12 +45,12 @@ class Test(unittest.TestCase):
                                       splitby=mbase.models_core.base.Split('X2', 'equiinterval')).isnull().all()[0],
                         'There should be no predictions for an independent variable conditioned on a dependent one')
 
-    # def test_compare_full_data_with_samples(self):
-    #     for var in mymod.data:
-    #         self.assertAlmostEqual(np.mean(mymod.data[var]), np.mean(mymod.samples[var]), 0,
-    #                                'Mean of data and posterior samples for ' + var + ' should be similar')
-    #         self.assertAlmostEqual(np.var(mymod.data[var]), np.var(mymod.samples[var]), 0,
-    #                                'Variance of data and posterior samples for ' + var + ' should be similar')
+    def test_compare_full_data_with_samples(self):
+        for var in mymod.data:
+            self.assertAlmostEqual(np.mean(mymod.data[var]), np.mean(mymod.samples[var]), 0,
+                                   'Mean of data and posterior samples for ' + var + ' should be similar')
+            self.assertAlmostEqual(np.var(mymod.data[var]), np.var(mymod.samples[var]), 0,
+                                   'Variance of data and posterior samples for ' + var + ' should be similar')
 
     def test_compare_conditioned_data_with_samples(self):
         covariates = [field['name'] for field in mymod.fields if field['independent']]
