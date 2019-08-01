8ce5eddb4f90b3f9f842ab02b2f7d85408885df5
jong42 <jonasguetter@gmx.de>
2019-08-01 13:40:33 +0200

    Fix minor bug

--- mb_modelbase/models_core/pyMC3_model.py
+++ mb_modelbase/models_core/pyMC3_model.py
@@ -301,7 +301,7 @@ class ProbabilisticPymc3Model(Model):
         #Copy shared_vars
         mycopy.shared_vars = {}
         if self.shared_vars:
-            for key, value in self.shared_vars:
+            for key, value in self.shared_vars.items():
                 mycopy.shared_vars[key] = theano.shared(value.get_value().copy())
         mycopy.nr_of_posterior_samples = self.nr_of_posterior_samples
         mycopy.fixed_data_length = self.fixed_data_length
