5d84126638b4d54206058c9b7ec942570dedba79
jong42 <jonasguetter@gmx.de>
2019-08-14 11:19:55 +0200

    Learn models once at the start instead of in each test method

--- mb_modelbase/models_core/tests/test_pyMC3.py
+++ mb_modelbase/models_core/tests/test_pyMC3.py
@@ -1,18 +1,22 @@
 import numpy as np
 import unittest
 import mb_modelbase.models_core.tests.create_PyMC3_testmodels as cr
+import copy
 
-def create_testmodels():
+def create_testmodels(fit):
     models = []
     # These functions return the model data and the corresponding model
-    models.append(cr.create_pymc3_simplest_model(fit=False))
-    models.append(cr.create_pymc3_getting_started_model(fit=False))
-    models.append(cr.create_pymc3_getting_started_model_independent_vars(fit=False))
-    models.append(cr.create_pymc3_coal_mining_disaster_model(fit=False))
-    models.append(cr.create_pymc3_eight_schools_model(fit=False))
-    models.append(cr.create_getting_started_model_shape(fit=False))
-    models.append(cr.create_flight_delay_model(fit=False))
+    models.append(cr.create_pymc3_simplest_model(fit=fit))
+    models.append(cr.create_pymc3_getting_started_model(fit=fit))
+    models.append(cr.create_pymc3_getting_started_model_independent_vars(fit=fit))
+    models.append(cr.create_pymc3_coal_mining_disaster_model(fit=fit))
+    models.append(cr.create_pymc3_eight_schools_model(fit=fit))
+    models.append(cr.create_getting_started_model_shape(fit=fit))
+    models.append(cr.create_flight_delay_model(fit=fit))
     return models
+
+models_unfitted = create_testmodels(fit=False)
+models_fitted = create_testmodels(fit=True)
 
 
 
@@ -25,7 +29,7 @@ class TestMethodsOnInitializedModel(unittest.TestCase):
         """
         Test if newly initialized model has data, test data or samples and if mode is set to None
         """
-        for data, mymod in create_testmodels():
+        for data, mymod in copy.deepcopy(models_unfitted):
             self.assertEqual(mymod.data.empty, 1, "There should be no data. Model:" + mymod.name)
             self.assertEqual(mymod.test_data.empty, 1, "There should be no test data. Model:" + mymod.name)
             self.assertEqual(mymod.samples.empty, 1, "There should be no samples. Model:" + mymod.name)
@@ -35,7 +39,7 @@ class TestMethodsOnInitializedModel(unittest.TestCase):
         """
         Test if data, test data and samples of the copied model are the same as in the original model
         """
-        for data, mymod in create_testmodels():
+        for data, mymod in copy.deepcopy(models_unfitted):
             mymod_copy = mymod.copy()
             self.assertEqual(mymod.data.equals(mymod_copy.data), 1,
                              "Copied model data is different than original model data. Model:" + mymod.name)
@@ -49,7 +53,7 @@ class TestMethodsOnInitializedModel(unittest.TestCase):
         Test if the set_data() method gives any data to the model
         and if the model data has the same columns as the input data and if mode is set to data
         """
-        for data, mymod in create_testmodels():
+        for data, mymod in copy.deepcopy(models_unfitted):
             mymod.set_data(data)
             self.assertEqual(mymod.data.empty, 0, "There is no data in the model. Model: " + mymod.name)
             self.assertEqual(mymod.data.columns.equals(data.columns), 1,
@@ -61,7 +65,7 @@ class TestMethodsOnInitializedModel(unittest.TestCase):
         Call _marginalizeout on a model without any samples.
         An error should be thrown since the model does not yet know any variables
         """
-        for data, mymod in create_testmodels():
+        for data, mymod in copy.deepcopy(models_unfitted):
             with self.assertRaises(ValueError):
                 mymod._marginalizeout(keep='A', remove='B')
             self.assertEqual(mymod.data.empty, 1, "There should be no data. Model: " + mymod.name)
@@ -74,7 +78,7 @@ class TestMethodsOnInitializedModel(unittest.TestCase):
         Call _conditionout on a model without any samples.
         An error should be thrown since the model does not yet know any variables
         """
-        for data, mymod in create_testmodels():
+        for data, mymod in copy.deepcopy(models_unfitted):
             with self.assertRaises(ValueError):
                 mymod._conditionout(keep='A', remove='B')
             self.assertEqual(mymod.data.empty, 1, "There should be no data. Model: " + mymod.name)
@@ -87,7 +91,7 @@ class TestMethodsOnInitializedModel(unittest.TestCase):
         Calculate a probability density on a model.
         An error should be thrown since the model does not yet know any variables
         """
-        for data, mymod in create_testmodels():
+        for data, mymod in copy.deepcopy(models_unfitted):
             with self.assertRaises(ValueError):
                 mymod.density([0])
 
@@ -95,7 +99,7 @@ class TestMethodsOnInitializedModel(unittest.TestCase):
         """
         Calculate the maximum probability of a model without samples. It should return an empty array
         """
-        for data, mymod in create_testmodels():
+        for data, mymod in copy.deepcopy(models_unfitted):
             self.assertTrue(len(mymod._maximum()) == 0,
                             "maximum density point for a model without variables should be an empty array. "
                             "Model: " + mymod.name)
@@ -110,7 +114,7 @@ class TestMethodsOnModelWithData(unittest.TestCase):
         """
         Test if data, test data and samples of the copied model are the same as in the original model
         """
-        for data, mymod in create_testmodels():
+        for data, mymod in copy.deepcopy(models_unfitted):
             mymod.set_data(data)
             mymod_copy = mymod.copy()
             self.assertEqual(mymod.data.equals(mymod_copy.data), 1,
@@ -124,7 +128,7 @@ class TestMethodsOnModelWithData(unittest.TestCase):
         """
         Test if there are samples, data and test data in the model and if the mode is set to both
         """
-        for data, mymod in create_testmodels():
+        for data, mymod in copy.deepcopy(models_unfitted):
             mymod.fit(data)
             self.assertEqual(mymod.data.empty, 0, "There is no data in the model. Model: " + mymod.name)
             self.assertEqual(mymod.test_data.empty, 0, "There is no test data in the model. Model: " + mymod.name)
@@ -140,7 +144,7 @@ class TestMethodsOnModelWithData(unittest.TestCase):
         Call _marginalizeout on a model without any samples for variables not in the model.
         An error should be thrown since the model does not have the variables
         """
-        for data, mymod in create_testmodels():
+        for data, mymod in copy.deepcopy(models_unfitted):
             mymod.set_data(data)
             with self.assertRaises(ValueError):
                 mymod._marginalizeout(keep='A', remove='B')
@@ -154,7 +158,7 @@ class TestMethodsOnModelWithData(unittest.TestCase):
         Call _conditionout on a model without any samples for variables not in the model.
         An error should be thrown since the model does not have the variables
         """
-        for data, mymod in create_testmodels():
+        for data, mymod in copy.deepcopy(models_unfitted):
             mymod.set_data(data)
             with self.assertRaises(ValueError):
                 mymod._conditionout(keep='A', remove='B')
@@ -166,7 +170,7 @@ class TestMethodsOnModelWithData(unittest.TestCase):
         """
         Calculate the maximum probability of a model without samples. It should return an empty array
         """
-        for data, mymod in create_testmodels():
+        for data, mymod in copy.deepcopy(models_unfitted):
             mymod.set_data(data)
             self.assertTrue(len(mymod._maximum()) == 0,
                             "maximum density point for a model without samples should be an empty array. "
@@ -181,8 +185,7 @@ class TestMethodsOnFittedModel(unittest.TestCase):
         """
         Test if data, test data and samples of the copied model are the same as in the original model
         """
-        for data, mymod in create_testmodels():
-            mymod.fit(data)
+        for data, mymod in copy.deepcopy(models_fitted):
             mymod_copy = mymod.copy()
             self.assertEqual(mymod.data.equals(mymod_copy.data), 1,
                              "Copied model data is different than original model data. Model: " + mymod.name)
@@ -195,8 +198,7 @@ class TestMethodsOnFittedModel(unittest.TestCase):
         """
         Call _marginalizeout on a fitted model. Check if the correct variables are removed from the model
         """
-        for data, mymod in create_testmodels():
-            mymod.fit(data)
+        for data, mymod in copy.deepcopy(models_fitted):
             keep = mymod.names[1:]
             remove = [mymod.names[0]]
             mymod._marginalizeout(keep=keep, remove=remove)
@@ -216,8 +218,7 @@ class TestMethodsOnFittedModel(unittest.TestCase):
         Call _conditionout on a fitted model. Check if the correct variables are removed from the model
         and if all the samples are within the variable domain
         """
-        for data, mymod in create_testmodels():
-            mymod.fit(data)
+        for data, mymod in copy.deepcopy(models_fitted):
             keep = mymod.names[1:]
             remove = [mymod.names[0]]
 
@@ -247,8 +248,7 @@ class TestMethodsOnFittedModel(unittest.TestCase):
         """
         Calculate a probability density on a model. A single scalar should be the return value
         """
-        for data, mymod in create_testmodels():
-            mymod.fit(data)
+        for data, mymod in copy.deepcopy(models_fitted):
             location = np.zeros(len(mymod.names))
             self.assertTrue(isinstance(mymod.density(location), float),
                             "A single scalar should be returned. Model: " + mymod.name)
@@ -257,14 +257,12 @@ class TestMethodsOnFittedModel(unittest.TestCase):
         """
         Calculate the maximum probability of a model. Dimensions should match
         """
-        for data, mymod in create_testmodels():
-            mymod.fit(data)
+        for data, mymod in copy.deepcopy(models_fitted):
             self.assertEqual(len(mymod._maximum()), len(mymod.names),
                              "Dimension of the maximum does not match dimension of the model. Model: " + mymod.name)
 
     def test_sample(self):
-        for data, mymod in create_testmodels():
-            mymod.fit(data)
+        for data, mymod in copy.deepcopy(models_fitted):
             n = 10
             self.assertEqual(mymod.sample(n).shape[0],  n, 'Number of samples is not correct')
 
@@ -282,8 +280,7 @@ class TestMoreCombinationsOnModel(unittest.TestCase):
         """
         Check if the density maximum of a marginalized model has the same dimensions as the model variables
         """
-        for data, mymod in create_testmodels():
-            mymod.fit(data)
+        for data, mymod in copy.deepcopy(models_fitted):
             remove = mymod.names[0]
             mymod.marginalize(remove=remove)
             self.assertEqual(len(mymod._maximum()), len(mymod.names),
