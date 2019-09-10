ed00453e38e7ea911105c93502342c3cd6aed093
jong42 <jonasguetter@gmx.de>
2019-09-10 11:18:10 +0200

    Fix number of posterior samples to length of data
    Previously, the number of posterior samples could be set arbitrarily.
    However, I noticed that this leads to a problem in models where
    dependent and independent variables interact with each other, for
    example when the sum of an independent and a dependent variable
    is computed. When the number of posterior samples differs from
    the number of data points, independent and dependent variables
    have different lengths, so the abovementioned operations cannot
    be computed and throw an error. This happens during the generation
    of posterior predictive samples. To fix that, the number of
    posterior samples are now fixed to the length of the data.

--- mb_modelbase/models_core/pyMC3_model.py
+++ mb_modelbase/models_core/pyMC3_model.py
@@ -37,7 +37,7 @@ class ProbabilisticPymc3Model(Model):
             new data points are generated with a different length than the original data
     """
 
-    def __init__(self, name, model_structure, shared_vars=None, nr_of_posterior_samples=500, fixed_data_length=False):
+    def __init__(self, name, model_structure, shared_vars=None, fixed_data_length=False):
         super().__init__(name)
         self.model_structure = model_structure
         self.samples = pd.DataFrame()
@@ -46,7 +46,6 @@ class ProbabilisticPymc3Model(Model):
         }
         self.parallel_processing = False
         self.shared_vars = shared_vars
-        self.nr_of_posterior_samples = nr_of_posterior_samples
         self.fixed_data_length = fixed_data_length
 
 
@@ -86,6 +85,10 @@ class ProbabilisticPymc3Model(Model):
                 'The following independent variables do not appear in shared_vars:' + str(missing_vars) + ' Make sure '\
                 'that you pass the data for each independent variable as theano shared variable to the constructor'
         self.check_data_and_shared_vars_on_equality()
+        # Set number of samples to the number of data points. The number of samples must not be chosen
+        # arbitrarily, since independent and dependent variables have to have the same dimensions. Otherwise,
+        # some models cannot compute posterior predictive samples
+        self.nr_of_posterior_samples = len(df)
         return ()
 
     def _generate_samples_for_independent_variable(self, key, size):
