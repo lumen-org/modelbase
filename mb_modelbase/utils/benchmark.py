cfac316b1992ef06cd5797c578cbd1e65d7bcd21
chrlen <christian.lengert@tuta.io>
2020-02-06 17:08:47 +0100

    Use Interaction log as Benchmark

--- mb_modelbase/utils/benchmark.py
+++ mb_modelbase/utils/benchmark.py
@@ -3,20 +3,21 @@ import numpy as np
 import pandas as pd
 import mb_modelbase.cache.cache as mc
 import time
+import json
 import abc
-
+import plotly.express as px
 
 class Benchmark(abc.ABC):
     def __init__(self,name):
         self.name = name
 
     @abc.abstractmethod
-    def _run(self, modelbase):
+    def _run(self, modelbase: mbase.ModelBase):
         """
             takes a modelbase and runs the benchmark
             @param modelbase a modelbase object
         """
-    def run(self,modelbase):
+    def run(self, modelbase: mbase.ModelBase) -> float:
         start = time.time()
         self._run(modelbase=modelbase)
         end = time.time()
@@ -25,43 +26,65 @@ class Benchmark(abc.ABC):
 class SimpleBenchmark(Benchmark):
     def __init__(self):
         Benchmark.__init__(self, 'Simple Benchmark')
-
-    def _run(self, modelbase):
-        queries = [
-            {'FROM': '__mcg_iris_map_0_0',
+        self.queries = [
+            {'FROM': 'mcg_allbus_map',
              'SPLIT BY': [{'name': 'sepal_length', 'split': 'equiinterval', 'args': [25], 'class': 'Split'}],
              'PREDICT': ['sepal_length', {'name': ['sepal_length'], 'aggregation': 'probability', 'class': 'Density'}]}
-
         ]
-        [modelbase.execute(q) for q in queries]
 
+    def _run(self, modelbase):
+        [modelbase.execute(q) for q in self.queries]
 
+class ActivityFileBenchmark(Benchmark):
+    def __init__(self, file='benchmarkInteraction2.log'):
+        Benchmark.__init__(self,name=file)
+        with open(file,'r') as f:
+            lines = list(f)
 
-if __name__ == '__name__':
+        noNewLine = list(map(lambda x: x[:-1], lines))
+        logsOnly = list(filter(lambda x: x != 'CACHE HIT!' and x != 'CACHE MISS!' and len(x.split('QUERY:')) == 2 , noNewLine))
+        msgOnly = list(map(lambda x: x.split('QUERY:')[1], logsOnly))
+        doubleQuotes = list(map(lambda x: x.replace("'", '"'), msgOnly))
+        jsons = list(map(json.loads, doubleQuotes))
+        self.queries = list(filter(lambda x: 'SHOW' not in x.keys(), jsons))
+        #self.queries = [json.loads(line) for line in lines]
+        #self.queries = self.queries + self.queries
+
+    def _run(self, modelbase):
+        [modelbase.execute(q) for q in self.queries]
+
+if __name__ == '__main__':
     no_cache = mbase.ModelBase(
         name='no_cache',
         model_dir='../../../fitted_models',
         cache=None)
 
     dict_cache = mbase.ModelBase(
-        name='no_cache',
+        name='dict_cache',
         model_dir='../../../fitted_models',
         cache=mc.DictCache())
 
-    mem_cache = mbase.ModelBase(
-        name='no_cache',
-        model_dir='../../../fitted_models',
-        cache=mc.MemcachedCache())
+    # mem_cache = mbase.ModelBase(
+    #     name='no_cache',
+    #     model_dir='../../../fitted_models',
+    #     cache=mc.MemcachedCache())
 
     bases = [
         no_cache,
-        dict_cache,
-        mem_cache
+        dict_cache
     ]
 
     benchmarks = [
-        SimpleBenchmark()
+        ActivityFileBenchmark
     ]
 
-    for ba in bases:
-        for be in benchmarks
+    results = pd.DataFrame({
+        base.name: [benchmark().run(base) for benchmark in benchmarks] for base in bases
+    })
+
+
+
+
+
+
+
