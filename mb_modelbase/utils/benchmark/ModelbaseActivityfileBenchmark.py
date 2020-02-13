22771a3ac2aa96a5e24515d5b0a1c81c2194baa9
chrlen <christian.lengert@tuta.io>
2020-02-13 14:59:21 +0100

    Adapt reading of interaction-files to new format

--- mb_modelbase/utils/benchmark/ModelbaseActivityfileBenchmark.py
+++ mb_modelbase/utils/benchmark/ModelbaseActivityfileBenchmark.py
@@ -8,14 +8,10 @@ class ModelbaseActivityfileBenchmark(Benchmark):
         with open(file,'r') as f:
             lines = list(f)
 
-        noNewLine = list(map(lambda x: x[:-1], lines))
-        logsOnly = list(filter(lambda x: x != 'CACHE HIT!' and x != 'CACHE MISS!' and len(x.split('QUERY:')) == 2 , noNewLine))
-        msgOnly = list(map(lambda x: x.split('QUERY:')[1], logsOnly))
-        doubleQuotes = list(map(lambda x: x.replace("'", '"'), msgOnly))
-        jsons = list(map(json.loads, doubleQuotes))
-        self.queries = list(filter(lambda x: 'SHOW' not in x.keys(), jsons))
-        #self.queries = [json.loads(line) for line in lines]
-        #self.queries = self.queries + self.queries
+        self.queries = [
+            json.loads(x[:-1])
+            for x in lines
+        ]
 
     def _run(self, instance: ModelBase) -> float:
         [instance.execute(q) for q in self.queries]
