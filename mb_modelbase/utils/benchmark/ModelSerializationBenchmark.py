9d76f230c5a06ec155688546f2b3182ea7197c26
chrlen <christian.lengert@tuta.io>
2020-02-24 11:17:19 +0100

    format

--- mb_modelbase/utils/benchmark/ModelSerializationBenchmark.py
+++ mb_modelbase/utils/benchmark/ModelSerializationBenchmark.py
@@ -2,13 +2,13 @@ from mb_modelbase.utils.benchmark.Benchmark import Benchmark
 from mb_modelbase.models_core.models import Model
 
 class ModelSerializationBenchmark(Benchmark):
-    def __init__(self,cache,n=100):
-        Benchmark.__init__(self, 'ModelSerializationBechmark',n=n)
+    def __init__(self, cache, n=1000):
+        Benchmark.__init__(self, 'ModelSerializationBechmark', n=n)
         self.cache = cache
 
     def _run(self, instance: Model):
         for i in range(self.n):
-            self.cache.set('test',instance)
+            self.cache.set('test', instance)
             res = self.cache.get('test')
 
 
