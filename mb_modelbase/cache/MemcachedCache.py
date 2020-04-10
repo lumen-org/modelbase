0e2344c74596a497aa725feaa190c6ae2cbee27e
chrlen <christian.lengert@tuta.io>
2020-04-10 07:32:25 +0200

    remove local storage of keys

--- mb_modelbase/cache/MemcachedCache.py
+++ mb_modelbase/cache/MemcachedCache.py
@@ -3,12 +3,15 @@ from pymemcache.client import base
 
 
 class MemcachedCache(BaseCache):
-    """
-    A cache with memcached as storage backend
+    """A cache with memcached as storage backend.
 
     Attributes:
-        hostname (str): hostname of memcached server
-        port (int): port of memcached server
+        _hostname (str): hostname of memcached server.
+        _port (int): port of memcached server.
+        _client (base.Client): The memcached client.
+    Args:
+        hostname (str): hostname of memcached server.
+        port (int): port of memcached server.
     """
 
     def __init__(self,
@@ -17,15 +20,11 @@ class MemcachedCache(BaseCache):
         BaseCache.__init__(self)
         self._hostname = hostname
         self._port = port
-        self._keys = {}
         self._client = base.Client((self._hostname, self._port))
 
     def _get(self, key, default=None):
         return self._client.get(key)
 
     def _set(self, key, data):
-        self._keys[key] = True
         self._client.set(key, data)
 
-    def keys(self):
-        return self._keys.keys()
