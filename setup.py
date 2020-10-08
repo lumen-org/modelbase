54085f69ab86440572df05356543e4d28297e0ed
Philipp Lucas <philipp.lucas@dlr.de>
2020-10-08 10:03:50 +0200

    fix missing ','

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(name='mb_modelbase',
       install_requires=[
           'anytree', #84 requires scipy==1.2, https://github.com/lumen-org/modelbase/issues/84
           'astropy',
-          'Cython'
+          'Cython',
           'dill',
           'flask>=1.1.1'
           'flask-cors',
@@ -21,7 +21,7 @@ setup(name='mb_modelbase',
           'multiprocessing_on_dill',
           'networkx',
           'numba',
-          'numpy==1.18.4'
+          'numpy==1.18.4',
           'pandas>=0.24',
           'prettytable',
           'pymc3',
