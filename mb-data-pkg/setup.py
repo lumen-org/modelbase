8b6f178a6162599eaef8af9c7a0b26f441bdc1b0
Philipp Lucas <philipp.lucas@dlr.de>
2020-12-01 13:01:40 +0100

    move tests to correct locations and make data namespace pkg

---
+++ mb-data-pkg/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_namespace_packages
+
+setup(name='mb-pymc3',
+      version='0.2',
+      description='pymc3 adapter to Lumens modelbase',
+      author='Philip Lucas, Christian Lengert, Jonas Gütter, Julien Klaus',
+      author_email='philipp.lucas@dlr.de, christian.lengert@dlr.de, Jonas.Aaron.Guetter@dlr.de, julien.klaus@uni-jena.de',
+      packages=find_namespace_packages(),
+      install_requires=[
+            'mb.modelbase',
+            'pymc3',
+            'numpy',
+            'pandas',
+            'scipy',
+      ],
+      zip_safe=False)
+
---
+++ mb-pymc3-pkg/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_namespace_packages
+
+setup(name='mb-pymc3',
+      version='0.2',
+      description='pymc3 adapter to Lumens modelbase',
+      author='Philip Lucas, Christian Lengert, Jonas Gütter, Julien Klaus',
+      author_email='philipp.lucas@dlr.de, christian.lengert@dlr.de, Jonas.Aaron.Guetter@dlr.de, julien.klaus@uni-jena.de',
+      packages=find_namespace_packages(),
+      install_requires=[
+            'mb.modelbase',
+            'pymc3',
+      ],
+      zip_safe=False)
+
