f0dad40792e1e4cea824c6edfb3d757fed5afd7a
Philipp Lucas <philipp.lucas@dlr.de>
2021-06-04 14:06:46 +0200

    fix preprocessing scripts

--- mb-data-pkg/mb/data/abalone/abalone.py
+++ mb-data-pkg/mb/data/abalone/abalone.py
@@ -3,11 +3,51 @@ import os
 
 _csvfilepath = os.path.splitext(__file__)[0] + ".csv"
 
-def data(file=_csvfilepath):
+import spn.structure.leaves.parametric.Parametric as spn_parameter_types
+import spn.structure.StatisticalTypes as spn_statistical_types
+
+
+def cg(file=_csvfilepath):
     df = pd.read_csv(file)
     df.dropna(axis=0, inplace=True)
     return df
 
+def spn_parameters():
+    abalone_variable_types = {
+        'Sex': spn_parameter_types.Categorical,
+        'Length': spn_parameter_types.Gaussian,
+        'Diameter': spn_parameter_types.Gaussian, 'Height': spn_parameter_types.Gaussian,
+        'WholeWeight': spn_parameter_types.Gaussian, 'ShuckedWeight': spn_parameter_types.Gaussian,
+        'VisceraWeight': spn_parameter_types.Gaussian, 'ShellWeight': spn_parameter_types.Gaussian,
+        'Rings': spn_parameter_types.Categorical
+    }
+    return abalone_variable_types
+
+def spn_metatypes():
+    abalone_variable_types = {
+        'Sex': spn_statistical_types.MetaType.DISCRETE,
+        'Length': spn_statistical_types.MetaType.REAL,
+        'Diameter': spn_statistical_types.MetaType.REAL, 'Height': spn_statistical_types.MetaType.REAL,
+        'WholeWeight': spn_statistical_types.MetaType.REAL, 'ShuckedWeight': spn_statistical_types.MetaType.REAL,
+        'VisceraWeight': spn_statistical_types.MetaType.REAL, 'ShellWeight': spn_statistical_types.MetaType.REAL,
+        'Rings': spn_statistical_types.MetaType.DISCRETE
+    }
+    return abalone_variable_types
+
+def mixed(filepath=_csvfilepath):
+    """Loads the abalone data set from a csv file, removes the index column and returns the
+    remaining data as a pandas data frame
+    """
+    df = pd.read_csv(filepath)
+    df.dropna(axis=0, inplace=True)
+    #df = df.replace({'M': 0, 'F': 1, 'I': 2})
+    df['Sex'] = df['Sex'].astype('category')
+    df['Rings'] = df['Rings'].astype('category')
+    return df
+
 
 if __name__ == "__main__":
-    df = data()
+    types = spn_statistical_types
+    print(types)
+
+
--- mb-data-pkg/mb/data/bank/bank.py
+++ mb-data-pkg/mb/data/bank/bank.py
@@ -7,6 +7,8 @@ Data Preprocessing and cleansing for the bank data set.
 
 import pandas as pd
 import os
+import spn.structure.leaves.parametric.Parametric as spn_parameter_types
+import spn.structure.StatisticalTypes as spn_statistical_types
 
 _csvfilepath = os.path.splitext(__file__)[0] + "-full.csv"
 
@@ -29,14 +31,14 @@ def mixed(datafilepath=_csvfilepath):
                      index_col=False, skipinitialspace=True)
 
     # drop NA/NaN
-    #datafilepath = 'data/adult/adult.full'
-    #df = pd.read_csv(datafilepath, index_col=False, na_values='?')
-    #dfclean = df.dropna()
+    # datafilepath = 'data/adult/adult.full'
+    # df = pd.read_csv(datafilepath, index_col=False, na_values='?')
+    # dfclean = df.dropna()
 
     # print information about columns:
     print("Columns and their data type:")
     for col in df.columns:
-        #print(df[col].name, " (", df[col].dtype, "), values counts: ", df[col].value_counts())
+        # print(df[col].name, " (", df[col].dtype, "), values counts: ", df[col].value_counts())
         print(df[col].name, " (", df[col].dtype)
 
     # print histograms on continuous columns
@@ -44,8 +46,41 @@ def mixed(datafilepath=_csvfilepath):
     return df
 
 
-def mixed_4cat(datafilepath=_csvfilepath):
+def spn_parameter_mixed():
+    bank_variable_types = {'age': spn_parameter_types.Gaussian, 'job': spn_parameter_types.Categorical,
+                           'marital': spn_parameter_types.Categorical, 'education': spn_parameter_types.Categorical,
+                           'default': spn_parameter_types.Categorical, 'balance': spn_parameter_types.Gaussian,
+                           'housing': spn_parameter_types.Categorical, 'loan': spn_parameter_types.Categorical,
+                           'contact': spn_parameter_types.Categorical, 'day': spn_parameter_types.Gaussian,
+                           'month': spn_parameter_types.Categorical, 'duration': spn_parameter_types.Gaussian,
+                           'campaign': spn_parameter_types.Gaussian, 'pdays': spn_parameter_types.Gaussian,
+                           'previous': spn_parameter_types.Gaussian, 'poutcome': spn_parameter_types.Categorical,
+                           'y': spn_parameter_types.Categorical}
+    return bank_variable_types
 
+
+def spn_metaparameters():
+    bank_meta_types = {'age': spn_statistical_types.MetaType.REAL,
+                       'job': spn_statistical_types.MetaType.DISCRETE,
+                       'marital': spn_statistical_types.MetaType.DISCRETE,
+                       'education': spn_statistical_types.MetaType.DISCRETE,
+                       'default': spn_statistical_types.MetaType.DISCRETE,
+                       'balance': spn_statistical_types.MetaType.REAL,
+                       'housing': spn_statistical_types.MetaType.DISCRETE,
+                       'loan': spn_statistical_types.MetaType.DISCRETE,
+                       'contact': spn_statistical_types.MetaType.DISCRETE,
+                       'day': spn_statistical_types.MetaType.REAL,
+                       'month': spn_statistical_types.MetaType.DISCRETE,
+                       'duration': spn_statistical_types.MetaType.REAL,
+                       'campaign': spn_statistical_types.MetaType.REAL,
+                       'pdays': spn_statistical_types.MetaType.REAL,
+                       'previous': spn_statistical_types.MetaType.REAL,
+                       'poutcome': spn_statistical_types.MetaType.DISCRETE,
+                       'y': spn_statistical_types.MetaType.DISCRETE}
+    return bank_meta_types
+
+
+def mixed_4cat(datafilepath=_csvfilepath):
     df = pd.read_csv(datafilepath, delimiter=";",
                      index_col=False, skipinitialspace=True)
     df = pd.DataFrame(df, index=None)
@@ -58,8 +93,9 @@ def mixed_4cat(datafilepath=_csvfilepath):
     del df['job']
     del df['poutcome']
     del df['marital']
-    
+
     df = df.dropna()
     df.reset_index(drop=True, inplace=True)
 
     return df
+
